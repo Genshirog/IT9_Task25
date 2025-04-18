9af4ae0d2b40900ae7b560c34aec81276c579a9e
Genshirog <imbaxgx1fromyt@gmail.com>
2025-04-18 22:32:28 +0800

    Arizobal_task25

---
+++ database/migrations/2025_04_18_074213_create_dependents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dependents', function (Blueprint $table) {
+            $table->bigIncrements('dependent_id');
+            $table->string('first_name',50);
+            $table->string('last_name',50);
+            $table->string('relationship',25);
+            $table->unsignedBigInteger('employee_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dependents');
+    }
+};
